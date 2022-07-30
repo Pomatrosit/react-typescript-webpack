6c0e9fd68289c0bbd4ef097c84cb5766b8e74a61
Vasilii Iurkin <vasiliiiurkin@MacBook-Air-Vasilii.local>
2022-07-30 10:08:49 +0300

    added wp loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import ReactLogo from "./react.png";
+import Vector from "./vector.svg";
+
 const App = () => {
-  return <h1>React Typescript Template</h1>;
+  return (
+    <div>
+      <h1>React Typescript Template</h1>
+      <img src={ReactLogo} alt="react-logo" />
+      <img src={Vector} alt="vector" />
+    </div>
+  );
 };
 
 export default App;
