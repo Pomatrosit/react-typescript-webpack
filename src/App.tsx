2aabd0027f3a0b36c30b6019b177e1a14179bf4e
Vasilii Iurkin <vasiliiiurkin@MacBook-Air-Vasilii.local>
2022-10-05 11:11:50 +0300

    remove build

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import ClickCounter from "./components/ClickCouter";
 import ReactLogo from "./react.png";
 import Vector from "./vector.svg";
 
 const App = () => {
   return (
     <div>
-      <h1>React Typescript Template</h1>
+      <h1>
+        React Typescript Template {process.env.NODE_ENV} {process.env.name}
+      </h1>
       <img src={ReactLogo} alt="react-logo" />
       <img src={Vector} alt="vector" />
+      <ClickCounter />
     </div>
   );
 };
---
+++ src/components/ClickCouter.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const ClickCounter = () => {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div>
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        Count: {count}
+      </button>
+    </div>
+  );
+};
+
+export default ClickCounter;
