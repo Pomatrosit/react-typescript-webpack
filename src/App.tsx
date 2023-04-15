645394016cae888dd80a76b27dd668c41fd33ff7
Vasilii Iurkin <vasiliiiurkin@MacBook-Air-Vasilii.local>
2023-04-15 09:07:00 +0300

    added separate env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import ClickCounter from "./components/ClickCouter";
-import ReactLogo from "./react.png";
-import Vector from "./vector.svg";
+import ReactLogo from "./assets/react.png";
+import Vector from "./assets/vector.svg";
 
 const App = () => {
   return (
     <div>
       <h1>
-        React Typescript Template {process.env.NODE_ENV} {process.env.name}
+        React Typescript {process.env.NODE_ENV} {process.env.NAME}
       </h1>
       <img src={ReactLogo} alt="react-logo" />
       <img src={Vector} alt="vector" />
